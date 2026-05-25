5d7592b0628bb5441aa2a0fd9bb7c5a1bc32b8dd
VolodimerT <49056205+VolodimerT@users.noreply.github.com>
2026-05-25 23:23:28 +0300

    Delete modules directory

--- modules/logger_setup.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def setup_logging(config_path: str = "bot_config.yaml"):
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(name)s | %(levelname)s | %(message)s")
-    return logging.getLogger()
-
-def get_logger(name: str):
-    return logging.getLogger(name)
